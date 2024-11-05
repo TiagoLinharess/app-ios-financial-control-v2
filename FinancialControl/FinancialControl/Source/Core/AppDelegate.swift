674a2f64f551f86d236daffbb2a6951b71445f4e
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-11-05 12:13:13 -0300

    Implements AppDelegate

---
+++ FinancialControl/FinancialControl/Source/Core/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FinancialControl
+//
+//  Created by Tiago Linhares on 05/11/24.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [
+            UIApplication.LaunchOptionsKey: Any
+        ]? = nil
+    ) -> Bool {
+        return true
+    }
+}
