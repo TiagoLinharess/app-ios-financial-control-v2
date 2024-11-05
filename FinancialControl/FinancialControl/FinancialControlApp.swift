f6dcd48aa6e42d0270e6aeb147e42e8b6e0c2239
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-11-05 10:34:42 -0300

    Start App

---
+++ FinancialControl/FinancialControl/FinancialControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialControlApp.swift
+//  FinancialControl
+//
+//  Created by Tiago Linhares on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinancialControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
