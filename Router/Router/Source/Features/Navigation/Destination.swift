459eea46efb42c43325eda969bc37d2599da6cb9
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-11-16 17:21:50 -0300

    Implements incoupled router

--- Router/Router/Source/Features/Navigation/Destination.swift
+++ Router/Router/Source/Features/Navigation/Destination.swift
@@ -7,50 +7,9 @@
 
 import SwiftUI
 
-public enum Destination: CaseIterable, Codable, Hashable {
-    case Home
-    case SingleForm
-    case PaymentType
-    case Products
-    case Budgets
-    case Bills
-    
-    public var title: String {
-        switch self {
-        case .Home:
-            "Home"
-        case .SingleForm:
-            "Single Form"
-        case .PaymentType:
-            "Payment Type"
-        case .Products:
-            "Products"
-        case .Budgets:
-            "Budgets"
-        case .Bills:
-            "Bills"
-        }
-    }
-    
-    public var iconName: String {
-        switch self {
-        case .Home:
-            "house"
-        case .SingleForm:
-            "dollarsign"
-        case .PaymentType:
-            "list.bullet"
-        case .Products:
-            "bookmark"
-        case .Budgets:
-            "calendar.day.timeline.left"
-        case .Bills:
-            "calendar"
-        }
-    }
-    
-    @ViewBuilder
-    public var view: some View {
-        Text(self.title)
-    }
+public protocol DestinationProtocol: CaseIterable, Codable, Hashable {
+    associatedtype ContentView: View
+    var title: String { get }
+    var iconName: String { get }
+    var view: ContentView { get }
 }
