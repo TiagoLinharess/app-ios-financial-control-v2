304b693420f5eb4a33bc804e91bbf47df0af4dc1
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-12-23 16:57:19 -0300

    Implements layout rules

--- Login/Login/Source/Features/CreateAccount/CreateAccountView.swift
+++ Login/Login/Source/Features/CreateAccount/CreateAccountView.swift
@@ -5,14 +5,19 @@
 //  Created by Tiago Linhares on 22/12/24.
 //
 
+import SharpnezCore
 import SharpnezDesignSystemSwiftUI
 import SwiftUI
 
 struct CreateAccountView: View {
+    // MARK: - Properties -
+    
     @State var name: String = ""
     @State var email: String = ""
     @State var password: String = ""
     @State var confirmPassword: String = ""
+    
+    // MARK: - Body -
     
     var body: some View {
         SHContainerView(title: LoginLocalizable.CreateAccount.title, font: .poppins) {
@@ -29,32 +34,95 @@ struct CreateAccountView: View {
                     color: .primarySH,
                     font: .body(.montserrat, .regular),
                     titleFont: .subtitle(.montserrat, .regular),
-                    text: $email
+                    text: $email,
+                    rules: [
+                        .init(
+                            title: LoginLocalizable.CreateAccount.emailRule,
+                            isComplete: email.isValidEmail
+                        )
+                    ],
+                    contentType: .emailAddress,
+                    keyboardType: .emailAddress
                 )
-                SHTextField(
+                SHPasswordField(
                     title: LoginLocalizable.CreateAccount.password,
                     color: .primarySH,
                     font: .body(.montserrat, .regular),
                     titleFont: .subtitle(.montserrat, .regular),
-                    text: $password
+                    text: $password,
+                    rules: [
+                        .init(
+                            title: LoginLocalizable.CreateAccount.numberCharactersRule,
+                            isComplete: numberCharactersRule()
+                        ),
+                        .init(
+                            title: LoginLocalizable.CreateAccount.capitalLetterRule,
+                            isComplete: password.containsUppercasedLetter
+                        ),
+                        .init(
+                            title: LoginLocalizable.CreateAccount.lowercaseLetterRule,
+                            isComplete: password.containsLowercasedLetter
+                        ),
+                        .init(
+                            title: LoginLocalizable.CreateAccount.numberRule,
+                            isComplete: password.containsNumber
+                        ),
+                        .init(
+                            title: LoginLocalizable.CreateAccount.specialCharacterRule,
+                            isComplete: password.containsSpecialCharacter
+                        ),
+                    ]
                 )
-                SHTextField(
+                SHPasswordField(
                     title: LoginLocalizable.CreateAccount.confirmPassword,
                     color: .primarySH,
                     font: .body(.montserrat, .regular),
                     titleFont: .subtitle(.montserrat, .regular),
-                    text: $confirmPassword
+                    text: $confirmPassword,
+                    rules: [
+                        .init(
+                            title: LoginLocalizable.CreateAccount.confirmPasswordRule,
+                            isComplete: passwordMustBeEqual()
+                        )
+                    ]
                 )
-                Spacer()
                 SHButton(
                     title: LoginLocalizable.CreateAccount.title,
                     style: .primary(.primarySH, .onPrimarySH),
                     font: .body(.montserrat, .regular),
+                    isDisabled: handleIsButtonDisabled(),
                     action: handleCreateAccount
                 )
             }
             .padding(.small)
         }
+    }
+    
+    // MARK: - Private Methods -
+    
+    private func numberCharactersRule() -> Bool {
+        return password.count >= 8
+    }
+    
+    private func passwordMustBeEqual() -> Bool {
+        password == confirmPassword && !password.isEmpty && !confirmPassword.isEmpty
+    }
+    
+    private func handlePasswordValidation() -> Bool {
+        passwordMustBeEqual() &&
+        numberCharactersRule() &&
+        password.containsNumber &&
+        password.containsUppercasedLetter &&
+        password.containsLowercasedLetter &&
+        password.containsSpecialCharacter
+    }
+    
+    private func handleIsButtonDisabled() -> Bool {
+        email.isEmpty ||
+        name.isEmpty ||
+        password.isEmpty ||
+        confirmPassword.isEmpty ||
+        !handlePasswordValidation()
     }
     
     private func handleCreateAccount() {
--- Login/Login/Source/Infrastructure/Localizable/Localizable.swift
+++ Login/Login/Source/Infrastructure/Localizable/Localizable.swift
@@ -22,6 +22,13 @@ enum LoginLocalizable {
         static let email = LoginLocalizable.tr("Localizable", "createAccount.email", fallback: "E-mail")
         static let password = LoginLocalizable.tr("Localizable", "createAccount.password", fallback: "Senha")
         static let confirmPassword = LoginLocalizable.tr("Localizable", "createAccount.confirmPassword", fallback: "Confirmar senha")
+        static let numberCharactersRule = LoginLocalizable.tr("Localizable", "createAccount.numberCharactersRule", fallback: "• Pelo menos de 8 caracteres")
+        static let capitalLetterRule = LoginLocalizable.tr("Localizable", "createAccount.capitalLetterRule", fallback: "• Pelo menos 1 letra maiúscula")
+        static let lowercaseLetterRule = LoginLocalizable.tr("Localizable", "createAccount.lowercaseLetterRule", fallback: "• Pelo menos 1 letra minúscula")
+        static let numberRule = LoginLocalizable.tr("Localizable", "createAccount.numberRule", fallback: "• Pelo menos 1 número")
+        static let specialCharacterRule = LoginLocalizable.tr("Localizable", "createAccount.specialCharacterRule", fallback: "• Pelo menos 1 caractere especial")
+        static let confirmPasswordRule = LoginLocalizable.tr("Localizable", "createAccount.confirmPasswordRule", fallback: "• As senhas precisam ser as mesmas")
+        static let emailRule = LoginLocalizable.tr("Localizable", "createAccount.emailRule", fallback: "• O e-mail precisa ser válido")
     }
 }
 
