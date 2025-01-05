d0db36ee46b2ab930775ce8a34914e6d5ea63c09
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-01-05 08:34:20 -0300

    Implements uikit structure

---
+++ FinancialControl/FinancialControl/Sources/Features/Start/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewController.swift
+//  FinancialControl
+//
+//  Created by Tiago Linhares on 04/01/25.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = Localizable.Start.login
+    }
+}
+
---
+++ Old/FinancialControl/FinancialControl/Source/Infrastructure/Localizable/Localizable.swift
@@ -0,0 +1,37 @@
+//
+//  Localizable.swift
+//  Home
+//
+//  Created by Tiago Linhares on 16/11/24.
+//
+
+import SwiftUI
+
+enum Localizable {
+    // MARK: - Start -
+    
+    enum Start {
+        static let title = Localizable.tr("Localizable", "start.title", fallback: "Financial Control")
+        static let login = Localizable.tr("Localizable", "start.login", fallback: "Login")
+        static let createAccount = Localizable.tr("Localizable", "start.createAccount", fallback: "Create Account")
+    }
+}
+
+// MARK: - Implementation Details -
+
+extension Localizable {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String = "") -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
