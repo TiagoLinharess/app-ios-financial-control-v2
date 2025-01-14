4dfe296e1e51e16e37e2a3f546cb284dd7005189
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-01-13 21:07:11 -0300

    Implements UI

--- Login/Login/Sources/Features/CreateAccount/CreateAccountView.swift
+++ Login/Login/Sources/Features/CreateAccount/CreateAccountView.swift
@@ -5,11 +5,174 @@
 //  Created by Tiago Linhares on 05/01/25.
 //
 
+import SharpnezCore
 import SharpnezDesignSystemUIKit
-
+import SnapKit
 
 protocol CreateAccountViewProtocol: UIView {
 }
 
 final class CreateAccountView: UISHContainerView, CreateAccountViewProtocol {
+    
+    // MARK: Properties
+    
+    private var emailRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.emailRule,
+        isComplete: false
+    )
+    
+    private var numberCharactersRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.numberCharactersRule,
+        isComplete: false
+    )
+    
+    private var capitalLetterRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.capitalLetterRule,
+        isComplete: false
+    )
+    
+    private var lowercaseLetterRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.lowercaseLetterRule,
+        isComplete: false
+    )
+    
+    private var numberRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.numberRule,
+        isComplete: false
+    )
+    
+    private var specialCharacterRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.specialCharacterRule,
+        isComplete: false
+    )
+    
+    private var confirmPasswordRule = UISHListItemViewModel(
+        title: LoginLocalizable.CreateAccount.confirmPasswordRule,
+        isComplete: false
+    )
+    
+    // MARK: UI elements
+    
+    private lazy var nameField: UISHTextField = UISHTextField(
+        title: LoginLocalizable.Commons.name,
+        font: .montserrat,
+        color: .onBackgroundSH
+    )
+    
+    private lazy var emailField: UISHTextField = {
+        let field = UISHTextField(
+            title: LoginLocalizable.Commons.email,
+            font: .montserrat,
+            color: .onBackgroundSH,
+            listItems: [emailRule]
+        )
+        field.contentType = .emailAddress
+        field.keyboardType = .emailAddress
+        return field
+    }()
+    
+    private lazy var passwordField: UISHPasswordField = UISHPasswordField(
+        title: LoginLocalizable.Commons.password,
+        font: .montserrat,
+        color: .onBackgroundSH,
+        listItems: [
+            numberCharactersRule,
+            capitalLetterRule,
+            lowercaseLetterRule,
+            numberRule,
+            specialCharacterRule
+        ]
+    )
+    
+    private lazy var confirmPasswordField: UISHPasswordField = UISHPasswordField(
+        title: LoginLocalizable.Commons.confirmPassword,
+        font: .montserrat,
+        color: .onBackgroundSH,
+        listItems: [confirmPasswordRule]
+    )
+    
+    private lazy var createAccountButton: UISHButton = UISHButton(
+        style: .primary(.primarySH, .onPrimarySH),
+        title: LoginLocalizable.CreateAccount.title,
+        font: .montserrat
+    )
+    
+    // MARK: Init
+    
+    override init() {
+        super.init()
+        setup()
+    }
+
+    @available(*, unavailable)
+    @MainActor required init?(coder: NSCoder) { nil }
+}
+
+extension CreateAccountView: ViewCode {
+    
+    // MARK: View Code
+    
+    func setupView() { }
+
+    func setupHierarchy() {
+        scrollView.addSubviews(
+            nameField,
+            emailField,
+            passwordField,
+            confirmPasswordField,
+            createAccountButton
+        )
+    }
+
+    func setupConstraints() {
+        nameField.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(CGFloat.small)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(CGFloat.small)
+        }
+        
+        emailField.snp.makeConstraints {
+            $0.top.equalTo(nameField.snp.bottom).offset(CGFloat.small)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(CGFloat.small)
+        }
+        
+        passwordField.snp.makeConstraints {
+            $0.top.equalTo(emailField.snp.bottom).offset(CGFloat.small)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(CGFloat.small)
+        }
+        
+        confirmPasswordField.snp.makeConstraints {
+            $0.top.equalTo(passwordField.snp.bottom).offset(CGFloat.small)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(CGFloat.small)
+        }
+        
+        createAccountButton.snp.makeConstraints {
+            $0.top.greaterThanOrEqualTo(confirmPasswordField.snp.bottom).offset(CGFloat.small)
+            $0.bottom.equalToSuperview().inset(CGFloat.small)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(CGFloat.small)
+        }
+    }
+    
+    func setupActions() {
+        emailField.onChange = { [weak self] textField in
+            guard let self else { return }
+            self.emailRule.isComplete = textField.text?.isValidEmail
+            self.emailField.updateListItems([self.emailRule])
+        }
+        
+        passwordField.onChange = { [weak self] textField in
+            guard let self else { return }
+            self.numberCharactersRule.isComplete = (textField.text?.count ?? .zero) > 7
+            self.capitalLetterRule.isComplete = textField.text?.containsUppercasedLetter
+            self.lowercaseLetterRule.isComplete = textField.text?.containsLowercasedLetter
+            self.numberRule.isComplete = textField.text?.containsNumber
+            self.specialCharacterRule.isComplete = textField.text?.containsSpecialCharacter
+            self.passwordField.updateListItems([
+                numberCharactersRule,
+                capitalLetterRule,
+                lowercaseLetterRule,
+                numberRule,
+                specialCharacterRule
+            ])
+        }
+    }
 }
--- Login/Login/Sources/Infrastructure/Localizable/LoginLocalizable.swift
+++ Login/Login/Sources/Infrastructure/Localizable/LoginLocalizable.swift
@@ -9,20 +9,85 @@ import SwiftUI
 
 enum LoginLocalizable {
     
+    // MARK: Commons
+    
+    enum Commons {
+        static let name = LoginLocalizable.tr("Localizable", "commons.name", fallback: "Name")
+        static let email = LoginLocalizable.tr("Localizable", "commons.email", fallback: "E-mail")
+        static let password = LoginLocalizable.tr("Localizable", "commons.password", fallback: "Password")
+        static let confirmPassword = LoginLocalizable.tr(
+            "Localizable",
+            "commons.confirmPassword",
+            fallback: "Confirm password"
+        )
+    }
+    
     // MARK: Create account
     
     enum CreateAccount {
-        static let title = LoginLocalizable.tr("Localizable", "createAccount.title", fallback: "Create Account")
+        static let title = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.title",
+            fallback: "Create Account"
+        )
+        static let numberCharactersRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.numberCharactersRule",
+            fallback: "At least 8 characters"
+        )
+        static let capitalLetterRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.capitalLetterRule",
+            fallback: "At least 1 capital letter"
+        )
+        static let lowercaseLetterRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.lowercaseLetterRule",
+            fallback: "At least 1 lowercase letter"
+        )
+        static let numberRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.numberRule",
+            fallback: "At least 1 number"
+        )
+        static let specialCharacterRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.specialCharacterRule",
+            fallback: "At least 1 special character"
+        )
+        static let confirmPasswordRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.confirmPasswordRule",
+            fallback: "Passwords must be equal"
+        )
+        static let emailRule = LoginLocalizable.tr(
+            "Localizable",
+            "createAccount.emailRule",
+            fallback: "E-mail must be valid"
+        )
     }
 }
 
 // MARK: Implementation Details
 
 extension LoginLocalizable {
-  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String = "") -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
-    return String(format: format, locale: Locale.current, arguments: args)
-  }
+    private static func tr(
+        _ table: String,
+        _ key: String,
+        _ args: CVarArg...,
+        fallback value: String = ""
+    ) -> String {
+        let locale = getPreferredLocale()
+        let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+        return String(format: format, locale: locale, arguments: args)
+    }
+    
+    static func getPreferredLocale() -> Locale {
+        guard let preferredIdentifier = Locale.preferredLanguages.first else {
+            return Locale.current
+        }
+        return Locale(identifier: preferredIdentifier)
+    }
 }
 
 private final class BundleToken {
